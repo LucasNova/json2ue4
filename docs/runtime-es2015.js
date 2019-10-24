53fb173bb0132b87a32042718a4c198b56dd70f1
LucasNova <pogorelowmax@gmail.com>
2019-10-25 01:51:06 +0600

    changed deploy url

--- docs/runtime-es2015.js
+++ docs/runtime-es2015.js
@@ -137,7 +137,7 @@
 /******/ 	__webpack_require__.o = function(object, property) { return Object.prototype.hasOwnProperty.call(object, property); };
 /******/
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "";
+/******/ 	__webpack_require__.p = "https://lucasnova.github.io/json2ue4/";
 /******/
 /******/ 	var jsonpArray = window["webpackJsonp"] = window["webpackJsonp"] || [];
 /******/ 	var oldJsonpFunction = jsonpArray.push.bind(jsonpArray);
